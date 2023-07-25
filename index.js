0032974e453ce10064634794b6e5923e5a1dd65d
not-nullptr <needhelpwithrift@gmail.com>
2023-07-25 16:19:56 +0100

    add .serve support for html

--- index.js
+++ index.js
@@ -36,15 +36,20 @@ const server = http.createServer(async (req, res) => {
             }
         );
     } else {
-        fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
-            if (err) {
-                res.writeHead(404, { "Content-Type": "text/plain" });
-                res.end("404 Not Found");
-            } else {
-                res.writeHead(200, { "Content-Type": "text/html" });
-                res.end(data);
+        fs.readFile(
+            fs.existsSync("index.html.serve")
+                ? "index.html.serve"
+                : "index.html",
+            (err, data) => {
+                if (err) {
+                    res.writeHead(404, { "Content-Type": "text/plain" });
+                    res.end("404 Not Found");
+                } else {
+                    res.writeHead(200, { "Content-Type": "text/html" });
+                    res.end(data);
+                }
             }
-        });
+        );
     }
 });
 
