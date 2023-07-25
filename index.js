3f8fedabfb8bf259d237a1c5022c2ab7faf4e630
not-nullptr <needhelpwithrift@gmail.com>
2023-07-25 01:45:24 +0100

    better spa support

--- index.js
+++ index.js
@@ -7,54 +7,51 @@ const process = require("process");
 const port = parseInt(process.argv[2]) || 3000;
 
 const server = http.createServer(async (req, res) => {
-    const url = req.url === "/" ? "index.html" : req.url;
-    const pathName = `${path.join(__dirname, path.normalize(url))}`;
-    console.log(pathName);
-    if (url.startsWith("/assets/")) {
-        fs.readFile(pathName, (err, data) => {
-            if (err) {
-                console.log(err);
-                console.log(pathName, url);
-                res.writeHead(404, { "Content-Type": "text/plain" });
-                res.end("404 Not Found");
-            } else {
-                const extension = path.extname(
-                    `${path.join(__dirname, pathName)}`
-                );
-                const contentType = getContentType(extension);
-                res.writeHead(200, { "Content-Type": contentType });
-                res.end(data);
-            }
-        });
-    } else {
-        fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
-            if (err) {
-                res.writeHead(404, { "Content-Type": "text/plain" });
-                res.end("404 Not Found");
-            } else {
-                res.writeHead(200, { "Content-Type": "text/html" });
-                res.end(data);
-            }
-        });
-    }
+	const url = req.url === "/" ? "index.html" : req.url;
+	const pathName = `${path.join(__dirname, path.normalize(url))}`;
+	console.log(pathName);
+	if (url.startsWith("/assets/")) {
+		fs.readFile(
+			url.endsWith(".js") && fs.existsSync(`${pathName}.serve`)
+				? `${pathName}.serve`
+				: pathName,
+			(err, data) => {
+				if (err) {
+					console.log(err);
+					console.log(pathName, url);
+					res.writeHead(404, {
+						"Content-Type": "text/plain",
+					});
+					res.end("404 Not Found");
+				} else {
+					const extension = path.extname(
+						`${path.join(__dirname, pathName)}`,
+					);
+					const contentType = getContentType(extension);
+					res.writeHead(200, {
+						"Content-Type": contentType,
+					});
+					res.end(data);
+				}
+			},
+		);
+	} else {
+		fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+			if (err) {
+				res.writeHead(404, { "Content-Type": "text/plain" });
+				res.end("404 Not Found");
+			} else {
+				res.writeHead(200, { "Content-Type": "text/html" });
+				res.end(data);
+			}
+		});
+	}
 });
 
 function getContentType(extension) {
-    // switch (extension) {
-    //     case ".html":
-    //         return "text/html";
-    //     case ".css":
-    //         return "text/css";
-    //     case ".js":
-    //         return "text/javascript";
-    //     case ".woff2":
-    //         return "font/woff2"
-    //     default:
-    //         return "text/plain";
-    // }
-    return mime.lookup(extension);
+	return mime.lookup(extension);
 }
 
 server.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+	console.log(`Server is running at http://localhost:${port}`);
 });
